8d44fd666fdab6dfc8c39e6a7b2cd2fde5a95227
Erinn <iamerinnnelson@gmail.com>
2019-10-15 23:13:17 -0400

    delete useless code

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,49 +33,6 @@ export default () => {
     }
   }
 
-  // const rightFindBox = (i, distance) => {
-  //   if (i % 4 === (3 - (distance - 1))) {
-  //     return null;
-  //   } else {
-  //     return i + distance;
-  //   }
-  // }
-
-  // const downFindBox = (i, distance) => {
-  //   if (i >= (12 - (4 * (distance - 1)))) {
-  //     return null;
-  //   } else {
-  //     return i + (4 * distance);
-  //   }
-  // }
-
-  // const leftFindBox = (i, distance) => {
-  //   if (i % 4 === (0 + (distance - 1))) {
-  //     return null;
-  //   } else {
-  //     return i - distance;
-  //   }
-  // }
-
-  const findX = (i) => {
-    if (i >= 12) {
-      return 0
-    }
-    if (i >= 8 && i < 12) {
-      return 1
-    }
-    if (i >= 4 && i < 8) {
-      return 2
-    }
-    if (i >= 0 && i < 4) {
-      return 3;
-    }
-  }
-
-  const findY = (i) => {
-    return i % 4;
-  }
-
   const movementStarter = (direction) => {
     if (direction === 'up') {
       return {
@@ -103,22 +60,15 @@ export default () => {
 
   const [playBoxes, setPlayBoxes] = useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(i => ({
     index: i,
-    // x: findX(i),
-    // y: findY(i),
     justDoubled: false,
     val: null,
   }
   )))
 
-  // const randomSixteen = () => {
-  //   return Math.floor(Math.random() * 16);
-  // }
 
   const fillSpecificBox = (i, val) => {
     let updatedBoxes = [...playBoxes]
     updatedBoxes[i] = { ...updatedBoxes[i], val: val }
-    // console.log(i)
-    // console.log(updatedBoxes)
     setPlayBoxes(updatedBoxes)
   }
 
@@ -143,82 +93,6 @@ export default () => {
 
   }
 
-  const checkDestination = (i, direction) => {
-    if (!playBoxes[i][`${direction}Ind`]) {
-      return false
-    } else if (playBoxes[i].val === playBoxes[i][`${direction}Val`]) {
-      return 'match';
-    } else if (!playBoxes[i][`${direction}Val`]) {
-      return 'open';
-    } else {
-      return false;
-    }
-  }
-
-  const destinationInfo = (i, direction, iRange) => {
-
-    let indexTwo;
-    let indexThree;
-    let indexFour;
-
-    if (iRange = 1) {
-      let indexTwo = playBoxes[i][`${direction}Ind`];
-      if (playBoxes[indexTwo].val && playBoxes[i].val !== playBoxes[indexTwo].val) {
-        return {
-          index: i,
-          val: playBoxes[i].val
-        }
-      }
-      if (playBoxes[indexTwo].val && playBoxes[i].val === playBoxes[indexTwo].val) {
-        return {
-          index: indexTwo,
-          val: playBoxes[i].val * 2
-        }
-      }
-      if (playBoxes[indexTwo].val === null && playBoxes[indexTwo][`${direction}Ind`]) {
-        return {
-          index: indexTwo,
-          val: playBoxes[i].val
-        }
-      }
-    }
-    if (iRange >= 2) {
-      let indexThree = playBoxes[indexTwo][`${direction}Ind`];
-      if (playBoxes[indexThree].val && playBoxes[i].val !== playBoxes[indexThree].val) {
-        return {
-          index: indexTwo,
-          val: playBoxes[i].val
-        }
-      }
-      if (playBoxes[indexThree].val && playBoxes[i].val === playBoxes[indexThree].val) {
-        return {
-          index: indexThree,
-          val: playBoxes[i].val * 2
-        }
-      }
-      if (playBoxes[indexThree].val === null && playBoxes[indexThree][`${direction}Ind`]) {
-        return {
-          index: indexThree,
-          val: playBoxes[i].val
-        }
-      }
-    }
-  }
-
-
-
-
-
-
-  const moveBoxRows = (i, direction, iRange) => {
-    let destinationPackage = destinationInfo(i, direction, iRange)
-    let updatedBoxes = [...playBoxes]
-    updatedBoxes[destinationPackage.index] = { ...updatedBoxes[destinationPackage.index], val: destinationPackage.val }
-    updatedBoxes[i] = { ...updatedBoxes[i], val: null }
-    setPlayBoxes(updatedBoxes)
-
-  }
-
   const fillRandomBox = () => {
     const emptyBoxes = playBoxes.filter(box => (
       !box.val
@@ -227,8 +101,6 @@ export default () => {
     const randomEmptyBoxInd = emptyBoxes[random].index;
     fillSpecificBox(randomEmptyBoxInd, newBoxValue())
   }
-
-
 
   const moveBoxes = (e) => {
     let input = getKeyCode(e)
@@ -239,7 +111,6 @@ export default () => {
       let newPlayBoxes = [];
       movementPackage.start.forEach(i => {
         let copyBox = { ...playBoxes[i] }
-        // console.log(playBoxes[findBoxIndex(i, movementPackage.moveback, 3)].index)
         let firstBoxBack = { ...playBoxes[findBoxIndex(i, movementPackage.moveback, 1)] };
         let secondBoxBack = { ...playBoxes[findBoxIndex(i, movementPackage.moveback, 2)] };
         let thirdBoxBack = { ...playBoxes[findBoxIndex(i, movementPackage.moveback, 3)] };
@@ -305,7 +176,7 @@ export default () => {
         } else {
           if (thirdBoxBack.val) {
             secondBoxBack.val = thirdBoxBack.val;
-            secondBoxBack.val = null;
+            thirdBoxBack.val = null;
           }
         }
         newPlayBoxes.push(copyBox, firstBoxBack, secondBoxBack, thirdBoxBack)
@@ -319,14 +190,9 @@ export default () => {
       const random = Math.floor(Math.random() * emptyBoxes.length);
       const randomEmptyBoxInd = emptyBoxes[random].index;
       newPlayBoxes[randomEmptyBoxInd].val = newBoxValue()
-
       setPlayBoxes(newPlayBoxes)
-
     }
-
   }
-
-
 
   const newBoxValue = () => {
     const probability = Math.random()
@@ -337,12 +203,9 @@ export default () => {
     }
   }
 
-
   useEffect(() => {
     fillRandomBox();
     console.log(playBoxes)
-
-
 
   }, [])
 
