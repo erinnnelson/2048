ec7b7b34a9563e55edd97c84658f7a6db4b65721
Erinn <iamerinnnelson@gmail.com>
2019-11-24 11:47:58 -0500

    refactor game logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -3,18 +3,96 @@ import Square from './Square';
 
 export default () => {
 
+  // SETTING BOARD VALUES
+
   const [playBoxes, setPlayBoxes] = useState([])
 
-  const emptyBoxes = () => {
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-  }
-
+  // Randomly assigns a value of 2 or 4 to a box
   const newBoxValue = () => {
     const probability = Math.random()
     if (probability < .9) {
       return 2;
     } else {
       return 4;
+    }
+  }
+  // Returns the index of a random empty box, skipIndex is a box to be skipped manually
+  const findEmptyBoxIndex = (boxes, skipIndex) => {
+    const emptyBoxes = boxes.filter(box => (
+      skipIndex === undefined ?
+        box.val === null
+        :
+        box.index !== skipIndex
+    ));
+    const rand = Math.floor(Math.random() * emptyBoxes.length);
+    return emptyBoxes[rand].index;
+  }
+
+  // Sets a value to a box by index
+  const setBoxValueByIndex = (boxes, value, i) => {
+    boxes[i] = { index: i, val: value }
+  }
+
+  // Creates an array from which to build an empty board of boxes
+  const boxBuild = () => {
+    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+  }
+
+  // Resets playBoxes in state by building a blank board and filling in two boxes
+  const startNewGame = () => {
+    const emptyBoxes = boxBuild().map(i => ({
+      index: i,
+      val: null,
+    }
+    ))
+    let firstIndex = findEmptyBoxIndex(emptyBoxes);
+    setBoxValueByIndex(emptyBoxes, newBoxValue(), firstIndex);
+    let secondIndex = findEmptyBoxIndex(emptyBoxes, firstIndex);
+    setBoxValueByIndex(emptyBoxes, newBoxValue(), secondIndex);
+    setPlayBoxes(emptyBoxes);
+  }
+
+  useEffect(() => {
+    startNewGame()
+
+  }, [])
+
+  // BOARD MOVEMENT
+
+  const getKeyCode = (e) => {
+    return e.keyCode || e.charCode || 0
+  }
+
+  const getMovementPack = (e) => {
+    const keyCode = getKeyCode(e);
+    if (keyCode === 38 || keyCode === 87) {
+      return {
+        direction: 'up',
+        moveBack: 'down',
+        starterIndices: [0, 1, 2, 3]
+      }
+    }
+    if (keyCode === 39 || keyCode === 68) {
+      return {
+        direction: 'right',
+        moveBack: 'left',
+        starterIndices: [3, 7, 11, 15]
+      }
+    }
+    if (keyCode === 40 || keyCode === 83) {
+      return {
+        direction: 'down',
+        moveBack: 'up',
+        starterIndices: [15, 14, 13, 12]
+      }
+    }
+    if (keyCode === 37 || keyCode === 65) {
+      return {
+        direction: 'left',
+        moveBack: 'right',
+        starterIndices: [12, 8, 4, 0]
+      }
+
     }
   }
 
@@ -49,120 +127,58 @@ export default () => {
     }
   }
 
-  const movementStarter = (direction) => {
-    if (direction === 'up') {
-      return {
-        start: [0, 1, 2, 3],
-        moveback: 'down'
-      }
-    } else if (direction === 'right') {
-      return {
-        start: [3, 7, 11, 15],
-        moveback: 'left'
-      }
-    } else if (direction === 'down') {
-      return {
-        start: [15, 14, 13, 12],
-        moveback: 'up'
-      }
-    } else if (direction === 'left') {
-      return {
-        start: [12, 8, 4, 0],
-        moveback: 'right'
-      }
-    }
-  }
-
-  const getKeyCode = (e) => {
-    return e.keyCode || e.charCode || 0
-  }
-
-  const findDirection = (input) => {
-    if (input === 38 || input === 87) {
-      return 'up'
-    }
-    if (input === 39 || input === 68) {
-      return 'right'
-    }
-    if (input === 40 || input === 83) {
-      return 'down'
-    }
-    if (input === 37 || input === 65) {
-      return 'left'
-    }
-  }
-
-  const getSecondBoxInd = (firstInd, emptyBoxCount) => {
-    let secondInd = Math.floor(Math.random() * emptyBoxCount);
-    if (secondInd === firstInd) {
-      secondInd = getSecondBoxInd(firstInd, emptyBoxCount);
-    }
-    return secondInd;
-
-  }
-
-  const fillTwoBoxes = (boxes) => {
-    const random1 = Math.floor(Math.random() * boxes.length);
-    const random2 = getSecondBoxInd(random1, boxes.length)
-    boxes[random1] = { index: random1, val: newBoxValue() }
-    boxes[random2] = { index: random2, val: newBoxValue() }
-    return boxes
-  }
-
   const moveBoxes = (e) => {
-    let input = getKeyCode(e)
-    let direction = findDirection(input)
-    if (direction) {
-      let movementPackage = movementStarter(direction)
+    let movementPack = getMovementPack(e);
+    if (movementPack) {
       let newPlayBoxes = [];
       let boxesMoved = false;
-      movementPackage.start.forEach(i => {
-        let copyBox = { ...playBoxes[i] }
-        let firstBoxBack = { ...playBoxes[findBoxIndex(i, movementPackage.moveback, 1)] };
-        let secondBoxBack = { ...playBoxes[findBoxIndex(i, movementPackage.moveback, 2)] };
-        let thirdBoxBack = { ...playBoxes[findBoxIndex(i, movementPackage.moveback, 3)] };
-        if (copyBox.val) {
+      movementPack.starterIndices.forEach(i => {
+        let boxRoot = { ...playBoxes[i] }
+        let firstBoxBack = { ...playBoxes[findBoxIndex(i, movementPack.moveBack, 1)] };
+        let secondBoxBack = { ...playBoxes[findBoxIndex(i, movementPack.moveBack, 2)] };
+        let thirdBoxBack = { ...playBoxes[findBoxIndex(i, movementPack.moveBack, 3)] };
+        if (boxRoot.val) {
           if (firstBoxBack.val) {
-            if (firstBoxBack.val === copyBox.val) {
-              copyBox.val *= 2
+            if (firstBoxBack.val === boxRoot.val) {
+              boxRoot.val *= 2
               firstBoxBack.val = null
               boxesMoved = true;
             }
           } else if (secondBoxBack.val) {
-            if (secondBoxBack.val === copyBox.val) {
-              copyBox.val *= 2
+            if (secondBoxBack.val === boxRoot.val) {
+              boxRoot.val *= 2
               secondBoxBack.val = null
               boxesMoved = true;
             }
           } else if (thirdBoxBack.val) {
-            if (thirdBoxBack.val === copyBox.val) {
-              copyBox.val *= 2
+            if (thirdBoxBack.val === boxRoot.val) {
+              boxRoot.val *= 2
               thirdBoxBack.val = null
               boxesMoved = true;
             }
           }
         } else {
           if (firstBoxBack.val) {
-            copyBox.val = firstBoxBack.val;
+            boxRoot.val = firstBoxBack.val;
             firstBoxBack.val = null;
             boxesMoved = true;
-            if (secondBoxBack.val && secondBoxBack.val === copyBox.val) {
-              copyBox.val *= 2;
+            if (secondBoxBack.val && secondBoxBack.val === boxRoot.val) {
+              boxRoot.val *= 2;
               secondBoxBack.val = null;
-            } else if (thirdBoxBack.val && thirdBoxBack.val === copyBox.val) {
-              copyBox.val *= 2;
+            } else if (thirdBoxBack.val && thirdBoxBack.val === boxRoot.val) {
+              boxRoot.val *= 2;
               thirdBoxBack.val = null;
             }
           } else if (secondBoxBack.val) {
-            copyBox.val = secondBoxBack.val;
+            boxRoot.val = secondBoxBack.val;
             secondBoxBack.val = null;
             boxesMoved = true;
-            if (thirdBoxBack.val && thirdBoxBack.val === copyBox.val) {
-              copyBox.val *= 2;
+            if (thirdBoxBack.val && thirdBoxBack.val === boxRoot.val) {
+              boxRoot.val *= 2;
               thirdBoxBack.val = null;
             }
           } else if (thirdBoxBack.val) {
-            copyBox.val = thirdBoxBack.val;
+            boxRoot.val = thirdBoxBack.val;
             thirdBoxBack.val = null;
             boxesMoved = true;
           }
@@ -213,11 +229,10 @@ export default () => {
             boxesMoved = true;
           }
         }
-        newPlayBoxes.push(copyBox, firstBoxBack, secondBoxBack, thirdBoxBack)
+        newPlayBoxes.push(boxRoot, firstBoxBack, secondBoxBack, thirdBoxBack)
       })
-
+      
       newPlayBoxes.sort((a, b) => (a.index > b.index) ? 1 : -1)
-
       if (boxesMoved) {
         const emptyBoxes = newPlayBoxes.filter(box => (
           !box.val
@@ -230,19 +245,7 @@ export default () => {
     }
   }
 
-  const startNewGame = () => {
-    const freshBoxes = emptyBoxes().map(i => ({
-      index: i,
-      val: null,
-    }
-    ))
-    setPlayBoxes(fillTwoBoxes(freshBoxes))
-  }
 
-  useEffect(() => {
-    startNewGame()
-
-  }, [])
 
   return (
     <div className='game-board-container'>
@@ -257,5 +260,7 @@ export default () => {
       <br />
       <button id='reset-button' onClick={startNewGame}>New Game</button>
     </div>
+
+
   )
 }
